13ab1e3c34175f4a90ea6c6896d02634c378df6b
Sharadh Rajaraman <r.sharadh@outlook.sg>
2025-09-05 23:40:13 +0100

    Change Vulkan-Hpp config
    - disable exceptions
    - disable struct constructors only

--- tools/cmake/scripts/vulkan-hpp-module.cmake
+++ tools/cmake/scripts/vulkan-hpp-module.cmake
@@ -8,5 +8,6 @@ target_sources(VulkanHppModule PUBLIC
 target_compile_features(VulkanHppModule PUBLIC cxx_std_23)
 target_link_libraries(VulkanHppModule PUBLIC Vulkan::Headers)
 target_compile_definitions(VulkanHppModule PUBLIC
-  VULKAN_HPP_NO_CONSTRUCTORS
+	VULKAN_HPP_NO_EXCEPTIONS
+	VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 )

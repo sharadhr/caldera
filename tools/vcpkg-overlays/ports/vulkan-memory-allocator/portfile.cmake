7db7c488cbffb66d8c80dc5bb1d39b7afb098779
Sharadh Rajaraman <r.sharadh@outlook.sg>
2025-12-26 16:45:08 +0000

    Add VMA as custom port
    - Update the port to latest master

---
+++ tools/vcpkg-overlays/ports/vulkan-memory-allocator/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator
+    REF "e722e57c891a8fbe3cc73ca56c19dd76be242759"
+    SHA512 de9fc6fedfeb45a41a7e17cc2a5119998c66798de56b95ce856da0d52cc80f26c717183840b5af6400b78471ac639445ca42192dda9c8f1e021953f134227d6c
+    HEAD_REF master
+)
+
+set(opts "")
+if(VCPKG_TARGET_IS_WINDOWS)
+  set(opts "-DCMAKE_INSTALL_INCLUDEDIR=include/vma") # Vulkan SDK layout!
+endif()
+
+set(VCPKG_BUILD_TYPE release) # header-only port
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS ${opts}
+
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME VulkanMemoryAllocator CONFIG_PATH "share/cmake/VulkanMemoryAllocator")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")

139a3fd2186082a84b2d243b84f98c0c6b500efb
Sharadh Rajaraman <r.sharadh@outlook.sg>
2025-12-29 23:30:30 +0000

    Build VVL in clang triplet with MSVC

--- tools/vcpkg-overlays/triplets/x64-windows-clang-static-md.cmake
+++ tools/vcpkg-overlays/triplets/x64-windows-clang-static-md.cmake
@@ -4,3 +4,8 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../tools/cmake/toolchains/clang-msvc.cmake")
 set(VCPKG_LOAD_VCVARS_ENV ON)
+
+if(PORT MATCHES "vulkan-validationlayers")
+	# Use MSVC toolchain for building Vulkan Validation Layers
+	set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../tools/cmake/toolchains/cl-msvc.cmake")
+endif()
